bce1c1a5ae7533f1bf5b43e4016466cf40511bea
Alex <114286426+AlexBaranovIT@users.noreply.github.com>
2023-07-17 08:24:33 +0300

    Update scrape_site_2.py

--- scrape_site_2.py
+++ scrape_site_2.py
@@ -12,10 +12,11 @@ class CrawlingSpider(CrawlSpider):
         Rule(LinkExtractor(allow="catalogue", deny="category"), callback="parse_item"),
     )
 
+    
     def parse_item(self, response):
         yield {
             "title": response.css(".product_main h1::text").get(),
             "price": response.css(".price_color::text").get(),
             "availability": response.css(".availability::text")[1].get().replace("\n", "").replace(" ", "")
         }
-      
+        
