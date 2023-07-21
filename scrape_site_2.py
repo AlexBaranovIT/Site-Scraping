24d4726c407f38e8dcb97dd3dc46ac8078624c60
Alex <114286426+AlexBaranovIT@users.noreply.github.com>
2023-07-21 08:20:38 +0300

    Update scrape_site_2.py

--- scrape_site_2.py
+++ scrape_site_2.py
@@ -2,17 +2,20 @@ from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
 
+#Create class for scraping
 class CrawlingSpider(CrawlSpider):
     name = "mycrawler"
     allowed_domains = ["toscrape.com"]
     start_urls = ["https://books.toscrape.com"]
 
+    #State the rules of scraping
     rules = (
         Rule(LinkExtractor(allow="catalogue/category")),
         Rule(LinkExtractor(allow="catalogue", deny="category"), callback="parse_item"),
     )
 
-    
+
+    #Create a function for scraping the item
     def parse_item(self, response):
         yield {
             "title": response.css(".product_main h1::text").get(),
